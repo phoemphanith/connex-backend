5ad7248c784279eb43244fded2f1db8e5f7f3fc2
PHOEM Phanith <61651733+phoemphanith@users.noreply.github.com>
2022-01-25 11:18:33 +0700

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api as Api;
+use App\Http\Controllers\API as Api;
 
 /*
 |--------------------------------------------------------------------------
